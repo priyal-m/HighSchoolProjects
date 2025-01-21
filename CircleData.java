5729039ab5b60589688c8e9de0348562429d54a0
priyal-m <150826868+priyal-m@users.noreply.github.com>
2025-01-21 12:37:33 -0500

    Add files via upload

---
+++ CircleData.java
@@ -0,0 +1,20 @@
+//Priyal mangla
+//September 23,2021
+// calculates and displays the area & circumference of a circle given the radius
+import java.util.Scanner;
+public class CircleData
+{
+    public static void main(String[] args)
+    {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Please enter the radius of a circle in inches.");
+    double radius = input.nextDouble();
+    
+    double area = Math.pow(radius, 2)*Math.PI;
+    double circum = (2)*(radius)*(Math.PI);
+    
+    System.out.println("For a circle with a radius of " + radius);
+    System.out.printf("The area is %6.3f square inches", area);
+    System.out.printf("\nThe circumference is  %6.3f inches", circum);
+    }
+}
